eec39f7827694fdd1a6f5e1d6a1c10390d73610a
mrojaczy <95350111@study.bsuir.by>
2021-06-01 00:16:57 +0300

    add models structure

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Movie, Rating, Genre, RentalCertificate
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Rating)
+admin.site.register(Genre)
+admin.site.register(RentalCertificate)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    genre = models.ManyToManyField(Genre)
+
+
+class RentalCertificate(models.Model):
+    movie = models.OneToOneField(Movie, on_delete=models.CASCADE, primary_key=True)
+    number = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ['number', 'country']
+        index_together = ['number', 'country']
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = ['user', 'movie']
+        index_together = ['user', 'movie']
+
+
